57f79719932a1fcf7a16ca4968636a884fde11e5
Hamza Qureshi <hamza7771.861@gmail.com>
2024-05-03 21:35:57 +0500

    Deployment Testing

--- datagen/settings.py
+++ datagen/settings.py
@@ -119,8 +119,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 import os
 
